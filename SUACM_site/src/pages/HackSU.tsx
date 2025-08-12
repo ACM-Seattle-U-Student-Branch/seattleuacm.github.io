2890a952e2bf75c98513e17f25842aaf94bf3fbb
Bryan Kim <wabkim360@gmail.com>
2025-08-11 21:46:21 -0700

    setup basic structure of routes

---
+++ SUACM_site/src/pages/HackSU.tsx
@@ -0,0 +1,14 @@
+// Info about HackSU
+import React from 'react';
+
+const HackSU: React.FC = () => {
+  return (
+    <div>
+      <h1>HackSU</h1>
+      <p>HackSU is our annual hackathon event.</p>
+      <p>Details about the event will be provided here.</p>
+    </div>
+  );
+};
+
+export default HackSU;
---
+++ SUACM_site/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// Landing page
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to SUACM</h1>
+      <p>Your hub for all things ACM</p>
+    </div>
+  );
+};
+
+export default Home;
